56486fdfdee6fc0aedb9e654968cdc60aaae19bb
paulocayres <paulo.cayres@gmail.com>
2018-09-12 11:01:57 -0300

    Com peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -29,12 +29,17 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
 	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 	
+	public void placePeice(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 	
-	
-	
-	
-	
-
 }
---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,39 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
+import chess.pieces.King;
+
+public class ChessMatch {
+	
+	private Board board;
+
+	public ChessMatch() {
+		board = new Board(8,8);
+		initialSetup();
+	}
+	
+	public ChessPiece[][] getPieces(){
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		
+		
+		for (int i=0; i< board.getRows(); i++) {
+			for (int j=0; j<board.getColumns();j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+			}
+		}
+		
+		return mat;
+		
+	}
+	
+	private void initialSetup() {
+		board.placePeice(new Rook(board, Color.White), new Position(2,1));
+		board.placePeice(new King(board, Color.Black), new Position(0,4));
+		
+	}
+	
+	
+
+}
